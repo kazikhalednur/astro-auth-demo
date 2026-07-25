9e964be8a7d635967492ee6ff30630abc3c7c616
Kazi Khaled Nur <kazikhalednur@gmail.com>
2026-07-25 06:44:01 +0600

    Add sign-out API endpoint to clear auth cookies

---
+++ src/pages/api/auth/signout.js
@@ -0,0 +1,8 @@
+// With `output: 'static'` configured:
+export const prerender = false;
+
+export const GET = async ({ cookies, redirect }) => {
+  cookies.delete("sb-access-token", { path: "/" });
+  cookies.delete("sb-refresh-token", { path: "/" });
+  return redirect("/signin");
+};
