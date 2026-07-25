9a7ba85a2223351ecb2f4432930fc2bb59a92e9a
Kazi Khaled Nur <kazikhalednur@gmail.com>
2026-07-25 07:20:04 +0600

    Type middleware auth locals in env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,17 @@
+/// <reference types="astro/client" />
+
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string
-  readonly SUPABASE_ANON_KEY: string
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
+}
+
+declare namespace App {
+  interface Locals {
+    email: string | null;
+    user: import("@supabase/supabase-js").User | undefined;
+  }
 }
